fa3073f4bd22bba4f29d118e88676987fd26179f
Carlos Cerda <cfcerda@uamv.edu.ni>
2023-10-06 20:06:10 -0600

    Servicio API

---
+++ AWService/API/Controllers/PostsJSONController.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API.Controllers
+{
+    public class PostsJSONController : Controller
+    {
+        // GET: PostsJSON
+      
+
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            var Json = await client.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+            var result = JsonConvert.DeserializeObject<List<apiCls>>(Json);
+
+            return View(result);
+        }
+    }
+}
---
+++ AWService/API/Models/apiCls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class apiCls
+    {
+        [Display(Name = "Id Usuario")]
+        public int userId { get; set; }
+        [Display(Name = "Id Post")]
+        public int id { get; set; }
+
+        [Display(Name = "Title")]
+        public string title { get; set; }
+
+        [Display(Name = "Comment")]
+        public string body { get; set; }
+
+    }
+}
